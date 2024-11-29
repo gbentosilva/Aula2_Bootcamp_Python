5d94f4e291a64d9a459c12f2ffd5f2c8e67a7880
gbentosilva <gabrielbento9812@gmail.com>
2024-11-29 16:31:10 -0300

    adição lista

--- exercicio_11.py
+++ exercicio_11.py
@@ -2,4 +2,8 @@ data = input("insira uma data no formato dd/mm/aaa: ")
 lista_data_separada = data.split("/")
 print (lista_data_separada)
 
+print(f"o elemento 1> {lista_data_separada[1]}")
+
 #o código anterior gera uma lista de elementos, gerando uma lista com 3 strings
+
+#lembrando que em python o elemento um é o segundo elemento
